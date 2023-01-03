e05e08308abfcdb9bf4e34a2c2802881374cde2c
Sokolov Innokenty <im@qbbr.io>
2023-01-03 12:26:10 +0800

    patch resetnmaster

--- dwm.c
+++ dwm.c
@@ -213,6 +213,7 @@ static void propertynotify(XEvent *e);
 static void quit(const Arg *arg);
 static Monitor *recttomon(int x, int y, int w, int h);
 static void removesystrayicon(Client *i);
+static void resetnmaster(const Arg *arg);
 static void resize(Client *c, int x, int y, int w, int h, int interact);
 static void resizebarwin(Monitor *m);
 static void resizeclient(Client *c, int x, int y, int w, int h);
@@ -1522,6 +1523,13 @@ removesystrayicon(Client *i)
 	if (ii)
 		*ii = i->next;
 	free(i);
+}
+
+void
+resetnmaster(const Arg *arg)
+{
+	selmon->nmaster = 1;
+	arrange(selmon);
 }
 
 void
