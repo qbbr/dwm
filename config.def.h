642416be69f4627625220df028c3de6f944206d3
Sokolov Innokenty <im@qbbr.io>
2023-01-03 18:23:00 +0800

    patch movestack

--- config.def.h
+++ config.def.h
@@ -1,6 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 #include "gaplessgrid.c"
 #include "bstack.c"
+#include "movestack.c"
 
 /* appearance */
 static const unsigned int borderpx       = 2;   /* border pixel of windows */
@@ -137,6 +138,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_u,      resetnmaster,   {0} },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
