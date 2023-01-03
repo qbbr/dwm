5e82830c5d3c5220afcd5e199826e71dff86b423
Sokolov Innokenty <im@qbbr.io>
2023-01-03 12:23:39 +0800

    patch bottommargin

--- dwm.c
+++ dwm.c
@@ -2149,6 +2149,9 @@ updatebarpos(Monitor *m)
 	m->wh = m->mh;
 	if (m->showbar) {
 		m->wh -= bh;
+		m->wh -= bottommargin;
+		if (!m->topbar)
+			m->wy += bottommargin;
 		m->by = m->topbar ? m->wy : m->wy + m->wh;
 		m->wy = m->topbar ? m->wy + bh : m->wy;
 	} else
